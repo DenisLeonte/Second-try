5cf3c9031f7826d683b4bdcefa11fcee412c9f16
MyBoyDoru <59029310+MyBoyDoru@users.noreply.github.com>
2021-02-13 17:25:31 +0200

    V1.1

--- Second try/main.cpp
+++ Second try/main.cpp
@@ -1,6 +1,7 @@
 /*
-V 1.0 Changelog:
-Added some kind of IO
+V 1.1 Changelog
+Made the IO better and separated the operations and the input
+Still have to do the loop
 */
 #include <iostream>
 #include <fstream>
@@ -12,37 +13,53 @@ using namespace std;
 ifstream in("script.denis");
 map <string, float> var;
 string operators = ":=+-/*%";
+string functions[] = { "var","print" };
 
-void inputFromFile()
+//The operations hub function
+float operations(float val1, float val2, char op)
 {
-	char op;
-	int val1,val2;
-	in >> val1 >> op >> val2;
 	if (strchr(operators.c_str(), op))
 	{
 		switch (op)
 		{
 		case '+':
-			cout << val1 + val2;
+			return val1 + val2;
 			break;
 		case '-':
-			cout << val1 - val2;
+			return val1 - val2;
 			break;
 		case '*':
-			cout << val1 * val2;
+			return val1 * val2;
 			break;
 		case '/':
-			cout << val1 / val2;
+			return val1 / val2;
 			break;
 		case '%':
-			cout << val1 % val2;
+			int val3 = val1, val4 = val2;
+			return val3 % val4;
 			break;
 		}
 	}
 }
 
+//The main input function
+void IO()
+{
+	string op;
+	in >> op;
+	//print
+	if (strcmp(op.c_str(), functions[1].c_str()) == 0)
+	{
+		float a, b;
+		char x;
+		in >> a >> x >> b;
+		cout << operations(a, b, x) << endl;
+	}
+}
+
 int main()
 {
-	inputFromFile();
+	string op;
+	IO();
 	return 0;
 }
